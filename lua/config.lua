8455db8c3a29a3133a7e0a914fd254bb2c059d88
lch <lchopn@gmail.com>
2022-08-23 21:51:27 +0800

    Add comment

--- lua/config.lua
+++ lua/config.lua
@@ -29,6 +29,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
+-- Initialization language server
 require'lspconfig'.clangd.setup{}
 -- $GOPATH/bin/gopls
 require'lspconfig'.gopls.setup{}
