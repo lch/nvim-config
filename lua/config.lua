ca71580544d3c13a41dcf81c4e8d82e50cfd4191
lch <lchopn@gmail.com>
2022-08-25 17:44:32 +0800

    Fix bugs

--- lua/config.lua
+++ lua/config.lua
@@ -39,42 +39,42 @@ local luasnip = require 'luasnip'
 local cmp = require 'cmp'
 cmp.setup {
   snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
+	expand = function(args)
+	  luasnip.lsp_expand(args.body)
+	end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+	['<C-d>'] = cmp.mapping.scroll_docs(-4),
+	['<C-f>'] = cmp.mapping.scroll_docs(4),
+	['<C-Space>'] = cmp.mapping.complete(),
+	['<CR>'] = cmp.mapping.confirm {
+	  behavior = cmp.ConfirmBehavior.Replace,
+	  select = true,
+	},
+	['<Tab>'] = cmp.mapping(function(fallback)
+	  if cmp.visible() then
+		cmp.select_next_item()
+	  elseif luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	  else
+		fallback()
+	  end
+	end, { 'i', 's' }),
+	['<S-Tab>'] = cmp.mapping(function(fallback)
+	  if cmp.visible() then
+		cmp.select_prev_item()
+	  elseif luasnip.jumpable(-1) then
+		luasnip.jump(-1)
+	  else
+		fallback()
+	  end
+	end, { 'i', 's' }),
   }),
   sources = {
-    { name = 'nvim_lsp' },                       -- from language server
-    { name = 'luasnip' },
-    { name = 'vsnip', keyword_length = 2 },      -- nvim-cmp source for vim-vsnip
-    { name = 'nvim_lsp_signature_help'},         -- display function signatures with current parameter emphasized
+	{ name = 'nvim_lsp' },					   -- from language server
+	{ name = 'luasnip' },
+	{ name = 'vsnip', keyword_length = 2 },	  -- nvim-cmp source for vim-vsnip
+	{ name = 'nvim_lsp_signature_help'},		 -- display function signatures with current parameter emphasized
   },
 }
 
@@ -85,13 +85,13 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'gopls', 'rust_analyzer' }
+local servers = { 'clangd', 'gopls' }
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup {
+		-- on_attach = my_custom_on_attach,
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
 end
 
 -- disable virtual_text (inline) diagnostics and use floating window
@@ -115,22 +115,29 @@ vim.diagnostic.config({
 
 
 -- Rust Tools Config & Init
-local rt = {
-    server = {
-        settings = {
-            on_attach = function(_, bufnr)
-                -- Hover actions
-                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                -- Code action groups
-                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-                require 'illuminate'.on_attach(client)
-            end,
-            ["rust-analyzer"] = {
-                checkOnSave = {
-                    command = "clippy"
-                }, 
-            },
-        }
+local rt = require("rust-tools")
+
+rt.setup({
+  tools = {
+    hover_actions = {
+      border = {
+        { "╭", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╮", "FloatBorder" },
+        { "│", "FloatBorder" },
+        { "╯", "FloatBorder" },
+        { "─", "FloatBorder" },
+        { "╰", "FloatBorder" },
+        { "│", "FloatBorder" },
+      },
     },
-}
-require('rust-tools').setup(rt)
+  },
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
