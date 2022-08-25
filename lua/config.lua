bfda2c478070f8d0277c6cb5815fc597ac323160
lch <lchopn@gmail.com>
2022-08-25 17:03:54 +0800

    Fix settings not applied issue

--- lua/config.lua
+++ lua/config.lua
@@ -85,10 +85,11 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'gopls' }
+local servers = { 'clangd', 'gopls', 'rust_analyzer' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
+    on_attach = on_attach,
     capabilities = capabilities,
   }
 end
