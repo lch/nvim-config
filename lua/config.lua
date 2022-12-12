f2c76b331bf428daf2473ac55a84e5302f4812ee
lch <lchopn@gmail.com>
2022-12-12 17:42:22 +0800

    Update nvim-cmp config

--- lua/config.lua
+++ lua/config.lua
@@ -79,8 +79,9 @@ cmp.setup {
 }
 
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local lspconfig = require('lspconfig')
 
